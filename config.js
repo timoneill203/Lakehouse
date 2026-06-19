5588be268d291afc33931b9d4d067aeb6fd64aaf
timoneill203 <timoneill203@gmail.com>
2026-06-19 13:54:25 -0400

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
 // Access is controlled by the table policies in schema.sql.
 
 export const SUPABASE_URL = "https://xagbzmxtqltkkgnswzkn.supabase.co/rest/v1/";
-export const SUPABASE_ANON_KEY = "sb_publishable_m0y_FrwtC56RaUgK9GyWWQ_RxQ0mZ7_";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhZ2J6bXh0cWx0a2tnbnN3emtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NTA4MzksImV4cCI6MjA5NzQyNjgzOX0.k3FffoglIg048wU0freGqtuAk7VvKS73mDNWJbTiQIA";
